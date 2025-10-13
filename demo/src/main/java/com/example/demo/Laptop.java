41ef9c9d343098892a6f82e4ab32c95863b498fb
Himanshu <himanshu.k.upadhyay05@gmail.com>
2025-10-14 00:02:15 +0530

    Structure_Added

---
+++ demo/src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop implements Computer{
+    @Override
+    public void compile() {
+        System.out.println("compiling in laptop !");
+    }
+}
